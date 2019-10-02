31e2f8627663fc6815bebc7f002eb5cd86adfd97
sunDalik <dali1999@mail.ru>
2019-10-02 18:30:53 +0300

    la2 done

--- Lab2/src/script.js
+++ Lab2/src/script.js
@@ -8,6 +8,15 @@ class CharNode {
     }
 }
 
+class HuffmanNode {
+    constructor(char = "", probability = 0, left = null, right = null) {
+        this.char = char;
+        this.probability = probability;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 function encode_shannon(nodes) {
     if (nodes.length === 1) return;
     if (nodes.length === 2) {
@@ -15,7 +24,7 @@ function encode_shannon(nodes) {
         nodes[1].shannon += "1";
         return;
     }
-    nodes.sort(function (a, b) {
+    nodes.sort((a, b) => {
         return b.probability - a.probability;
     }); // reverse sort by probability
     let totalP = 0;
@@ -30,7 +39,7 @@ function encode_shannon(nodes) {
         halfTotalP += nodes[i].probability;
     }
     const diff1 = halfTotalP - (totalP - halfTotalP);
-    const diff2 = Math.abs(diff1 - (2 * nodes[i].probability));
+    const diff2 = Math.abs(halfTotalP - nodes[i].probability - (totalP - halfTotalP + nodes[i].probability));
     if (diff2 < diff1) i++;
     for (let j = 0; j < i; ++j) {
         nodes[j].shannon += "0"
@@ -42,10 +51,29 @@ function encode_shannon(nodes) {
     encode_shannon(nodes.slice(i));
 }
 
-function encode_huffman(nodes) {
-/*
+function encode_huffman(huffNodes) {
+    if (huffNodes.length === 1) {
+        return huffNodes[0];
+    }
+    huffNodes.sort((a, b) => {
+        return a.probability - b.probability;
+    });
+    huffNodes.push(new HuffmanNode(null, huffNodes[0].probability + huffNodes[1].probability, huffNodes[0], huffNodes[1]));
+    huffNodes = huffNodes.slice(2);
+    return encode_huffman(huffNodes);
+}
 
- */
+function assignCodes(huffNode, nodesList, code = "") {
+    if (huffNode.left === null && huffNode.right === null) {
+        for (const node of nodesList) {
+            if (node.char === huffNode.char) {
+                node.huffman = code;
+                return;
+            }
+        }
+    }
+    assignCodes(huffNode.left, nodesList, code + "0");
+    assignCodes(huffNode.right, nodesList, code + "1");
 }
 
 document.getElementById('file-input').oninput = function () {
@@ -57,6 +85,7 @@ document.getElementById('file-input').oninput = function () {
             <td>Shannon-Fano code</td>
             <td>Huffman code</td>
             </tr>`;
+        document.getElementById("avg").innerText = "";
         const fileReader = new FileReader();
         fileReader.onload = function (e) {
             let nodesList = [];
@@ -81,20 +110,31 @@ document.getElementById('file-input').oninput = function () {
                 nodesList[nodeI].frequency++;
             }
 
-            for (let i = 0; i < nodesList.length; ++i) {
-                const node = nodesList[i];
+            for (const node of nodesList) {
                 node.probability = node.frequency / sum;
             }
             encode_shannon(nodesList);
-            encode_huffman();
-            for (let i = 0; i < nodesList.length; ++i) {
-                const node = nodesList[i];
+            let huffNodesList = [];
+            for (const node of nodesList) {
+                huffNodesList.push(new HuffmanNode(node.char, node.probability))
+            }
+
+            assignCodes(encode_huffman(huffNodesList), nodesList, "");
+            let shannonLength = 0;
+            let huffmanLength = 0;
+            let charCount = 0;
+            for (const node of nodesList) {
+                shannonLength += node.probability * node.shannon.length;
+                huffmanLength += node.probability * node.huffman.length;
+                charCount++;
                 const newRow = document.getElementById("result").insertRow(); // add a row to the table
                 newRow.innerHTML = `<td>${node.char}</td>
                                     <td>${node.probability.toFixed(4)}</td>
                                     <td>${node.shannon}</td>
                                     <td>${node.huffman}</td>`
             }
+            document.getElementById("avg").innerText = `Average symbol length (Shannon-Fano): ${shannonLength.toFixed(4)} 
+            Average symbol length (Huffman): ${huffmanLength.toFixed(4)}`
         };
         fileReader.readAsText(file);
     }
